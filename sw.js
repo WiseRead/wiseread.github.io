768d2e8caa8b57447a7141ac1c621efeff3267d3
DragonWhisperer <noaragono15@gmail.com>
2021-01-23 22:18:25 +0200

    Make PWA site

---
+++ sw.js
@@ -0,0 +1,28 @@
+const cacheName = "pwa-conf-v1";
+const staticAssets = [
+    "./", 
+    "./index.html",
+    "./about.html",
+    "./app.js",
+    "./jquery-3.4.1.min.js",
+    "./jszip.js",
+    "./jszip.min.js",
+    "./loading-bar.css",
+    "./loading-bar.js"
+];
+
+self.addEventListener("install", async (event) => {
+  const cache = await caches.open(cacheName);
+  await cache.addAll(staticAssets);
+});
+
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+  event.respondWith(cacheFirst(req));
+});
+
+async function cacheFirst(req) {
+  const cache = await caches.open(cacheName);
+  const cachedResponse = await cache.match(req);
+  return cachedResponse || fetch(req);
+}
